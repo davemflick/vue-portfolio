93164fe627c335f3583954a5277a9a1260fbfcc9
davemflick <davidflick@Davids-iMac.local>
2018-05-05 11:55:33 -0400

    Basic setup before starting build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import { routes } from './routes.js';
+import VueResource from 'vue-resource';
+import store from './store/store.js';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+const router = new VueRouter({
+	mode: 'history',
+	routes: routes
+});
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
